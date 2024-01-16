8b9a48ae4eca9e026a76ec3c0f08261c615dc238
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-16 12:52:52 -0600

    Justin Nguyen | could not complete #1

--- homework.js
+++ homework.js
@@ -19,6 +19,8 @@ let person3 = {
 }
 
 
+displayFavoriteFoods(person3);
+
 //=======Exercise #2=========//
 /*
 Write an object prototype for a Person that has a name and age, has a
@@ -31,13 +33,40 @@ age by 3 years. Use an arrow function for both methods
 
 // Create our Person Prototype
 
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}  
 
 // Use an arrow to create the printInfo method
 
+this.printInfo = () => {
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+  };
+
 // Create another arrow function for the addAge method that takes a single parameter
+
+this.addAge = () => {
+    this.age += 1;
+  };
 // Adding to the age 
 
+this.addAgeBy = (years) => {
+    this.age += years;
+  }
 
+
+  const person1 = new Person("Justin", 27)
+  const person2 = new Person("David", 61)
+    
+  person1.printInfo()
+  person2.printInfo()
+    
+  person1.addAgeBy(5)
+    
+  console.log(person1.printInfo())
+  console.log(person2.printInfo())
+  
 // =============Exercise #3 ============//
 /*
 
@@ -46,3 +75,40 @@ age by 3 years. Use an arrow function for both methods
     If the length of the string is less than 10 console log "Small Number"
 */
 
+function checkLength(str) {
+    if (str.length > 10) {
+      console.log("Big word");
+    } else {
+      console.log("Small Number");
+    }
+  }
+
+  checkLength("THIS IS A TEST MESSAGE")
+  checkLength("HELLO")
+
+
+// CodeWars Problem
+
+// 1. Grasshopper - Personalized Message
+
+//   function greet(name, owner) {
+//     if (name == owner) {
+//         return 'Hello boss';
+//     } else {
+//         return 'Hello guest';
+//     }
+// }
+
+
+// 2. Counting sheep...
+
+//  function count_sheeps(num_of_sheep) {
+//     count = 0;
+//     for (s in num_of_sheep) {
+//         if (s == true) {
+//             count += 1;
+//     return count;
+//         }
+//     }
+
+// }
